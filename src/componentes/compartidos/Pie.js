2c8cb01775cda5a014f01182064fcae5a2f77c2f
Brian <sbriam@gmail.com>
2022-12-13 23:13:37 -0500

    tailwind installation

---
+++ src/componentes/compartidos/Pie.js
@@ -0,0 +1,9 @@
+function Pie() {
+    return (
+      <footer className="text-red-500"> 
+        <small>Developed and designed by Brian Saint Clair.©️2022.All rights reserved.</small>
+      </footer>
+    );
+  }
+  
+  export default Pie;
