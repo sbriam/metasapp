365896eb8ba3514ce05cd08c7b49d222d699ba1d
Brian <sbriam@gmail.com>
2022-12-13 23:16:36 -0500

    tailwind installation

---
+++ src/componentes/compartidos/Principal.js
@@ -0,0 +1,16 @@
+function Principal( { children }) {
+    return (
+      <>
+        <sidenav>
+          <a href="/lista">Lista</a>
+          <a href="/crear">Crear</a>
+        </sidenav>
+        <main>
+          {children}
+        </main>
+      </>
+    );
+  }
+  
+  export default Principal;
+  
